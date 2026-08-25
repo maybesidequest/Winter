10a6e1c8554b1fbcc280eda57c65a83677713d83
dev-737 <73829355+dev-737@users.noreply.github.com>
2026-08-25 13:35:17 +0530

    chore(db): trim unused core database tables from Winter drizzle schema

--- test_db.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./app/db.server";
-import { user } from "./drizzle/schema";
-async function run() {
-  const users = await db.select().from(user).limit(1);
-  console.log(users);
-  process.exit(0);
-}
-run();
